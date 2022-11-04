2026a38e7be4987a3a954bcc98aaa35091175655
Ashley <ashleynjeri7@gmail.com>
2022-11-04 07:51:58 +0300

    code-along

---
+++ code_along.rb
@@ -0,0 +1,15 @@
+=begin
+
+    AGGREGATE FUNCTIONS
+***************************
+
+1. SELECT AVG(column_name) FROM table_name;
+
+2. SELECT SUM(column_name) FROM table_name;
+
+3. SELECT MIN(column_name) FROM table_name;
+   SELECT MAX(column_name) FROM table_name;
+
+4. SELECT COUNT(column_name) FROM table_name;
+
+=end
